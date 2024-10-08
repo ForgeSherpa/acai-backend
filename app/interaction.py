d2afc6294da99243447a3f6827620b57b67603f1
albetnov <ryxinfrvr@gmail.com>
2024-10-08 15:30:46 +0700

    chore: update scaffold

--- app/interaction.py
+++ app/interaction.py
@@ -1,11 +1,20 @@
 from .data import ModelResponse, TableLookup, FilterLookup
 
-def queryModel(q: str) -> ModelResponse:
-    # WIP: Implement the query model function based on AI model response.
-    return ModelResponse(tables=[
-        TableLookup(
-            filters=[FilterLookup(
 
-            )]
-        )
-    ])
+def query_model(q: str) -> ModelResponse:
+    # WIP: Implement the query model function based on AI model response.
+    return ModelResponse(
+        tables=[
+            TableLookup(
+                columns=["field1"],
+                filters=[
+                    FilterLookup(
+                        column="field1",  # must match with columns above
+                        operator="=",
+                        value="value1",
+                    ),
+                ],
+                relations=["table2"],
+            ),
+        ]
+    )
