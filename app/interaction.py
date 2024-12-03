6430d7268f144f5d983af6639bbaa82f374bb4c9
Albet Novendo <76980465+albetnov@users.noreply.github.com>
2024-12-03 19:29:04 +0700

    Update interaction.py

--- app/interaction.py
+++ app/interaction.py
@@ -5,7 +5,13 @@ def query_model(q: str) -> ModelResponse:
     res = requests.post("http://localhost:5005/model/parse", json={"text": q})
     data = res.json()
 
+    intent = data['intent']['name']
+    entities = {entity['entity']: entity['value'] for entity in data['entities']}
+
+    print(f"query_model intent: {intent}")
+    print(f"query_model entities: {entities}")
+
     return ModelResponse(
-        intent=data['intent']['name'],
-        entities = {entity['entity']: entity['value'] for entity in data['entities']}
+        intent=intent,
+        entities=entities
     )
