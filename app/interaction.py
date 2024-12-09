7bb4e7893d857bd989a0475acbf43864caf45803
albetnov <ryxinfrvr@gmail.com>
2024-12-09 11:34:54 +0700

    cache model query

--- app/interaction.py
+++ app/interaction.py
@@ -1,9 +1,16 @@
 from .data import ModelResponse
 import requests
+import functools
+from time import time
+
+@functools.lru_cache()
+def request_model(q: str, ttl_hash=None):
+    res = requests.post("http://localhost:5005/model/parse", json={"text": q})
+    return res.json()
 
 def query_model(q: str) -> ModelResponse:
-    res = requests.post("http://localhost:5005/model/parse", json={"text": q})
-    data = res.json()
+    # cache 5 minutes
+    data = request_model(q, ttl_hash=round(time() / 300))
 
     intent = data['intent']['name']
     entities = {entity['entity']: entity['value'] for entity in data['entities']}
